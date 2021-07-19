eb7a83060b4b136f73e3436ed5ff5a5c02e24198
santoshrachakonda7 <santosh.rnsk@msitprogram.net>
2021-07-19 07:02:35 +0530

    triangleareabycoordinates is done

--- 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
+++ 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
@@ -6,4 +6,17 @@
 
 def triangleareabycoordinates(x1, y1, x2, y2, x3, y3):
 	# your code goes here
-	pass
+	#distance betwwen points
+	d1 = (((x2 - x1) ** 2) + ((y2 - y1) ** 2)) ** 0.5
+	d2 = (((x3 - x2) ** 2) + ((y3 - y2) ** 2)) ** 0.5
+	d3 = (((x1 - x3) ** 2) + ((y1 - y3) ** 2)) ** 0.5
+	return areaoftriangle(d1, d2, d3)
+
+def areaoftriangle(s1, s2, s3):
+	if((s1 + s2 <= s3) or (s2 + s3 <= s1) or (s3 + s1 <= s2)):
+		return 0
+	else:
+		# heron's formula
+		z = (s1 + s2 + s3)/2
+		a = ((z * (z - s1) * (z - s2) * (z - s3)) ** 0.5)
+		return a
