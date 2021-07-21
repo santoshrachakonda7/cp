3e2d794c3b3aee0b4df21d0cf955519bf9efa094
santoshrachakonda7 <santosh.rnsk@msitprogram.net>
2021-07-21 20:35:29 +0530

    ismostlymagicsquare is done

--- 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
+++ 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
@@ -15,4 +15,25 @@
 
 def ismostlymagicsquare(a):
 	# Your code goes here
-	pass
+	count_d1 = 0
+	count_d2 = 0
+	l1 = []
+	l2 = []
+	if(len(a) == len(a[0])):
+		for i in range(0, len(a)):
+			count_d1 += a[i][i]
+			count_d2 += a[i][len(a)-i-1]
+			r = 0
+			c = 0
+			for j in range(0, len(a[0])):
+				r += a[i][j]
+				c += a[j][i]
+			l1.append(r)
+			l2.append(c)
+			l1 = list(set(l1))
+			l2 = list(set(l2))
+
+			if(len(l1) == len(l2) == 1 and 
+			l1[0] == l2[0] == count_d1 == count_d2):
+				return True
+	return False
