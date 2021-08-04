d0018ddc05c5fe8239247686d69eecdae7aa3647
santoshrachakonda7 <santosh.rnsk@msitprogram.net>
2021-08-04 17:02:40 +0530

    nth_smithnumber is done

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -7,6 +7,55 @@
 # so fun_nthsmithnumber(0) should return 4
 # so fun_nthsmithnumber(1) should return 22
 
+def isprime(p):
+	if(p==2 or p==3):
+		return True
+	if(p<2 or p%2==0):
+		return False
+	if(p>3):
+		for i in range(3,p//2):
+			if(p%i==0):
+				return False
+		return True
+
+def issmith(n):
+	if(isprime(n)!=True):
+		t = n
+		fl = []
+		s = 0
+		if(n == 1):
+			return False
+		for i in range(2,(n//2) + 1):
+			if(isprime(i) == True) and (n%i == 0):
+				fl.append(i)
+		a = 0
+		for j in str(n):
+			a += int(j)
+		li = []
+		for i in fl:
+			while (t%i == 0 and t != 0):
+				li.append(i)    
+				s += i
+				t = t//i
+		b = 0        
+		for j in li:
+			if(j > 9):
+				for d in str(j):
+					b += int(d)
+			else:
+			   b+=j
+		if(a == b):
+			return True
+		else:
+			return False
 
 def fun_nth_smithnumber(n):
-    return 1
+	c=0
+	f=1
+	r = 0
+	while(n+1 != c):
+		if(issmith(f) == True):
+			c+=1
+			r = f
+		f = f + 1
+	return r
