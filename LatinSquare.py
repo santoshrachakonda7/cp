50d5611828c03d6f1a0d8d3fab35afde0621c137
santoshrachakonda7 <santosh.rnsk@msitprogram.net>
2021-08-08 20:41:19 +0530

    isLatinSquare is done

--- LatinSquare.py
+++ LatinSquare.py
@@ -9,4 +9,14 @@
 
 def isLatinSquare(lst):
     # Your code goes here...
-    pass
+    s = set(j for i in lst for j in i)
+    t = zip(*lst)
+    for i,j in zip(lst,t):
+        for k in s:
+            if k not in i or k not in j:
+                return False
+    return True
+
+print(isLatinSquare([["x", "y", "z"],["z", "x", "y"],
+["y", "z", "x"]]))
+print(isLatinSquare([[1, 1, 1], [1, 2, 2], [1, 3, 3], [2, 1, 2], [2, 2, 3], [2, 3, 1], [3, 1, 3], [3, 2, 1], [3, 3, 2]]))
